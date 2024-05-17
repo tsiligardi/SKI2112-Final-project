3d053e79a436ca71959a8323efbe5a4374697c27
tsiligardi <124154570+tsiligardi@users.noreply.github.com>
2024-05-17 12:25:20 +0200

    Using sympson integration tecniques

--- final_project.py
+++ final_project.py
@@ -1,13 +1,24 @@
 import numpy as np
 import pandas as pd
 import scipy
+import math
+
+
+
+#testing
+data=pd.read_csv('test.csv')
+latitude = data["Latitude"].to_numpy()
+longitude = data["Longitude"].to_numpy()
+a_tot=np.sqrt(data["ax"].to_numpy()**2+data["ay"].to_numpy()**2+data["az"].to_numpy()**2)
+time=data['time'].to_numpy()
+speed=data['Speed (m/s)'].to_numpy()
 
 
 
 def average_speed(total_acceleration,time):
     #To calculate the average speed, we can use the integral-mean theorem:
     #v_avg=1/(t_final-t_initial)*int^(t_final)_(t_initial)(total_acceleration)dt
-    v_avg=1/(time[len(time)-1]-time[0])*scipy.integrate.trapezoid(total_acceleration,time)
+    v_avg=1/(time[len(time)-1]-time[0])*scipy.integrate.simpson(total_acceleration,time)
     return v_avg
 
 
@@ -21,9 +32,7 @@ def get_velocity(total_acceleration,time):
 
 def get_total_distance(velocity,time):
     #One way to calculate the total distance, is by taking the integral of the velocity
-    total_distance=scipy.integrate.trapezoid(velocity,time)
+    total_distance=scipy.integrate.simpson(velocity,time)
     return total_distance
     
-
-
 
