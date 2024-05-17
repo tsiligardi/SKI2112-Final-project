acf07dff628cbc9f0cdc3ed726ebcb0fa22516bb
tsiligardi <124154570+tsiligardi@users.noreply.github.com>
2024-05-17 11:52:21 +0200

    Update final_project.py
    Created functions to calculate av. speed, the velocity and an attempt to calculate the total distance

--- final_project.py
+++ final_project.py
@@ -1,3 +1,29 @@
 import numpy as np
 import pandas as pd
+import scipy
+
+
+
+def average_speed(total_acceleration,time):
+    #To calculate the average speed, we can use the integral-mean theorem:
+    #v_avg=1/(t_final-t_initial)*int^(t_final)_(t_initial)(total_acceleration)dt
+    v_avg=1/(time[len(time)-1]-time[0])*scipy.integrate.trapezoid(total_acceleration,time)
+    return v_avg
+
+
+def get_velocity(total_acceleration,time):
+    #for a small interval dt
+    #v(t_2)=a(t_1)*dt+v(t_1)
+    v=np.zeros(len(total_acceleration))
+    for i in range((len(total_acceleration)-1)):
+        v[i+1]=(total_acceleration[i]*(time[i+1]-time[i]))+v[i]
+    return v
+
+def get_total_distance(velocity,time):
+    #One way to calculate the total distance, is by taking the integral of the velocity
+    total_distance=scipy.integrate.trapezoid(velocity,time)
+    return total_distance
+    
+
+
 
