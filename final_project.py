87e19f1f5f7c2c13c7ddf1a91836090314f537f1
tsiligardi <124154570+tsiligardi@users.noreply.github.com>
2024-05-24 12:42:06 +0200

    Created function to determine the mean on transport  (Rasmussen et al, 2015)

--- final_project.py
+++ final_project.py
@@ -4,18 +4,18 @@ import scipy
 import math
 import matplotlib.pyplot as plt
 
-# Testing
-# test_data='Test_data/bike Beek station.csv'
-# data=pd.read_csv(test_data)
-# latitude=data["Latitude"].to_numpy()
-# longitude=data['Longitude'].to_numpy()
-# speed=data['Speed (m/s)'].to_numpy()
-# time=data["time"].to_numpy()
-# ax=data["ax"].to_numpy()
-# ay=data["ay"].to_numpy()
-# az=data["az"].to_numpy()
-# a=np.sqrt(ax**2+ay**2+az**2)
 
+def moving_median_keep_ends(signal, window=3):
+    assert window % 2 != 0, "Give an odd number for the window"
+    smoothed = np.zeros_like(signal)
+    for i, point in enumerate(signal):
+        if i < window // 2 + 1:
+            smoothed[i] = np.median(signal[: i + window // 2 + 1])
+        elif len(smoothed) - i < window // 2 + 1:
+            smoothed[i] = np.median(signal[i - window // 2 - 1:])
+        else:
+            smoothed[i] = np.median(signal[i - window // 2 - 1:i + window // 2 + 1 ])
+    return smoothed
 
 def getDistance(lat1,lon1,lat2,lon2):
     # This uses the haversine formula, which remains a good numberical computation,
@@ -53,17 +53,69 @@ def average_speed(total_distance,total_time):
 
 def return_stats(data):
     #get some statistical values
-    av=np.average(data)
-    data=np.trim_zeros(data)
-    max_val = np.max(data)
     median_val = np.median(data)
-    sd=np.std(data)
-    val_85 = max_val*0.85
-    return(av,max_val,median_val,sd,val_85);
+    p95= np.percentile(data,95)
+    return(median_val,p95);
 
 
+#Rasmussen et al., Improved methods to deduct trip legs and mode from travel surveys using wearable GPS devices: 
+#A case study from the Greater Copenhagen area, 2015
+def mode_of_transport(p95_speed,p95_acc,median_speed):
+        if (median_speed>=8.5):
+            return 'car'
+        elif (median_speed >=6):
+            if p95_acc>0.32:
+          
+                if p95_speed>=8.5:
+                    return 'bus'
+                else:
+                    return 'car'
+            elif p95_speed>=8.5:
+                return 'car'
+            else:
+                return 'bike'
+        elif (median_speed>=2):
+            if p95_acc>=0.32:
+                if (p95_speed>2.5 and p95_speed<8.5):
+                    return 'car'
+                else:
+                    return 'bus'
+            elif p95_acc>=0.14:
+                if p95_speed>=8.5:
+                    return 'bus'
+                else: 
+                    return 'bike'
+            elif p95_speed<2.5:
+                return'walk'
+            else:
+                return 'bike'
+        else:
+            if p95_acc>=0.32:
+                return 'car'
+            elif p95_speed>=8.5:
+                return'bike'
+            else:
+                return'walk'
+                
+        
+# Testing
+# test_data='Test_data/test2.csv'
+# data=pd.read_csv(test_data)
+# latitude=data["Latitude"].to_numpy()
+# longitude=data['Longitude'].to_numpy()
+# speed=data['Speed (m/s)'].to_numpy()
+# time=data["time"].to_numpy()
+# ax=data["ax"].to_numpy()
+# ay=data["ay"].to_numpy()
+# az=data["az"].to_numpy()
+# a=np.sqrt(ax**2+ay**2)
 
 
+# median_acc,p95_acc=return_stats(a)
+# median_speed,p95_speed=return_stats(speed)
+# print(mode_of_transport(p95_speed, p95_acc, median_speed))
+   
+# plt.plot(a)
 
 
 
